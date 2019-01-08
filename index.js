87ead8a85eeec3892c8c23e7898349abbd574bd9
Martaalex <40301963+Martaalex@users.noreply.github.com>
2019-01-08 15:17:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var started = false;
 var randomZodis = '';
 var answerArray = [];
-var wrongLetter = 0;
+var wrongLetter = [];
 var answer = '';
+var lives = 5;
 
 const zodziuArr = ['Miestas', 'Mašina', 'Mokykla', 'Daina', 'Vaivorykštė', 'Pilis', 'Pelėda', 'Katinas', 'Kopūstai', 'Kamštis', 'Voverė', 'Laivas', 'Enciklopėdija', 'Pasaulis', 'Striukė', 'Parmezanas'];
 
@@ -17,23 +18,44 @@ var buttons = () => {
    var list = document.createElement('button');
     list.className = 'letter';
     list.value = alphabet[i];
-    list.addEventListener('click', e => { 
-     randomZodis.toUpperCase().split('').forEach((raide, index) => {
-      if (raide.indexOf(e.target.value) != -1 ){ 
-      console.log(index, raide)
-      answerArray[index] = raide;     
-       answer = answerArray.join(''); 
-       write.textContent = answer;
+
+    list.addEventListener('click', spejimas => { 
+     let count = 0;
+     spejimas.target.setAttribute('disabled',true);
+     console.log(spejimas)
+      randomZodis.toUpperCase().split('').forEach((raide, index) => {
+      if (raide.indexOf(spejimas.target.value) != -1 ){ 
+       answerArray[index] = raide;     
+            answer = answerArray.join(''); 
+       writeAnswer.textContent = answer;
+       count++;
       }  
-      if (answer === randomZodis.toUpperCase()){
-      write.textContent = `Tu atspėjai žodį - ${answer}`;
+     if (answer === randomZodis.toUpperCase()){
+      writeAnswer.textContent = `Tu atspėjai žodį - ${answer}`;
         }
-      });
+     });
+
+      if (count === 0){
+        wrongLetter.push(spejimas.target.value);
+        lives--;
+        writeLives.textContent = 'Tu turi dar ' + lives + ' bandymus.' ;
+        console.log(wrongLetter)
+          }
+          if (lives===0){
+            writeLives.textContent = 'Neatspejai =( , žodis buvo :' + randomZodis ;
+            removeBtn();
+           // playAgain();
+          }
      })
     list.textContent = alphabet[i];
     myButtons.appendChild(letters);
     letters.appendChild(list);
   }
+}
+
+const removeBtn = () =>{
+    var btnContainer = document.querySelector('#alphabet');
+    btnContainer.remove();
 }
 
 const app = document.getElementById('app');
@@ -49,15 +71,33 @@ function randomNumber() {
   for (var i = 0; i < randomZodis.length; i++) {
     answerArray[i] = " _ ";
   }
-  write.textContent = answerArray;
+  writeAnswer.textContent = answerArray ;
+  writeLives.textContent = 'Tu turi dar ' + lives + ' bandymus.' ;
  if(!started){
+
    buttons();
    startBtn.style.display = 'none';
+   restartBtn.style.display = 'block';
   }
   started=true;
+  
 };
 
+const restart = () =>{
+  
+};
 
-var write = document.createElement('div');
-app.appendChild(write);
+const restartBtn = document.createElement('button');
+restartBtn.setAttribute("id", "restart");
+restartBtn.textContent = 'restart';
+app.appendChild(restartBtn);
+restartBtn.style.display = 'none';
+restartBtn.addEventListener('click', restart) 
+
+
+  var writeAnswer = document.createElement('div');
+app.appendChild(writeAnswer);            
+var writeLives = document.createElement('div');
+app.appendChild(writeLives);    
+
 
